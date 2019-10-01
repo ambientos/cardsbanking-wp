07aace1d83d0a0f5869e95c04e00e2677b722cdc
ambientos <ant.grakhov@gmail.com>
2019-10-01 17:41:07 +0500

    Replace tinymce from parent theme, remove unused

--- themes/cardsbanking/functions.php
+++ themes/cardsbanking/functions.php
@@ -46,6 +46,8 @@ function cb_init() {
 	
 	$remove_filters = array(
 		array('wp_nav_menu', 'remove_current_links_from_menu', PHP_INT_MAX, 2),
+		array('mce_external_plugins', 'wpshop_enqueue_plugin_scripts', 10, 1),
+		array('mce_buttons_3', 'wpshop_register_buttons_editor', 10, 1),
 	);
 
 	foreach ($remove_filters as $remove_filter) {
@@ -54,6 +56,41 @@ function cb_init() {
 }
 
 add_action( 'init', 'cb_init' );
+
+
+/**
+ * Replace tinymce.js
+ */
+
+function cb_enqueue_plugin_scripts( $plugin_array ) {
+	$plugin_array['blockquote_button_plugin'] = get_stylesheet_directory_uri() . '/js/tinymce-plugin.js';
+
+	return $plugin_array;
+}
+
+add_filter( 'mce_external_plugins', 'cb_enqueue_plugin_scripts' );
+
+
+function cb_register_buttons_editor( $buttons ) {
+    //register buttons with their id.
+    array_push(
+        $buttons,
+        //'col_6',
+        //'col_4',
+        'blockquote_warning',
+        'blockquote_info',
+        'blockquote_danger',
+        'blockquote_check',
+        'blockquote_quote',
+        'blockquote_important',
+        'content_btn',
+        'spoiler_btn',
+        'mark_btn',
+        'mask_link'
+    );
+    return $buttons;
+}
+add_filter( 'mce_buttons_3', 'cb_register_buttons_editor', 11 );
 
 
 /**
